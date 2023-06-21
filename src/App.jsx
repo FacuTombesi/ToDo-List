0f8a302ffd451d3d144c63006f41006b4546cd73
FacuTombesi <facundomtombesi@gmail.com>
2023-06-21 18:09:18 -0300

    local storage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ToDoList } from "./components/ToDoList";
 import { ToDoForm } from "./components/ToDoForm";
 import "./styles.css";
 
 export default function App() {
-  const [todos, setTodos] = useState([]); // Y creo un estado para la lista de items
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS"); // Tomo los valores de localStorage y los guardo en una variable
+    if (localValue == null) return []; // Si esa variable esta vacia, seteo el useState inicial en un arrayt vacio
+    return JSON.parse(localValue); // De lo contrario, devuelvo lo que tenia en localStorage
+  }); // Creo un estado para la lista de items
+
+  // useEffect se encarga del localStorage para evitar que se pierdan datos cada vez que se recarga la pagina
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos)) // Cada vez que se ejecuta useEffect guardo los items nuevos en el localStorage
+  }, [todos] /* Cada vez que todos cambia se ejecuta useEffect */);
 
   const addTodo = (title) => {
     setTodos((currentTodos) => {
